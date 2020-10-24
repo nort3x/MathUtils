89d5bdd52699b617208e2a11497f8b4af57406c9
Unwaged_Brothers <H>
2020-10-24 04:11:53 +0330

    recommit

--- library.cpp
+++ library.cpp
@@ -1,22 +1,23 @@
 #include "library.h"
 #include <iostream>
+#include <utility>
 
 
 
-double Differentiation::Forward1D(RealFunction1D function, StepSize s, Point p) {
+double Differentiation::Forward1D(const RealFunction1D& function, StepSize s, Point p) {
     return (function(p + s) - function(p))/s;
 }
-double Differentiation::Backward1D(RealFunction1D function, StepSize s, Point p) {
+double Differentiation::Backward1D(const RealFunction1D& function, StepSize s, Point p) {
     return Differentiation::Forward1D(function,-s,p);
 }
-double Differentiation::ThreePointMidPoint1D(RealFunction1D function, StepSize s, Point p) {
+double Differentiation::ThreePointMidPoint1D(const RealFunction1D& function, StepSize s, Point p) {
     return (function(p+s)-function(p-s))/(2*s);
 }
-double Differentiation::FivePointMidPoint1D(RealFunction1D function, StepSize s, Point p) {
+double Differentiation::FivePointMidPoint1D(const RealFunction1D& function, StepSize s, Point p) {
 
     return (double(4)*ThreePointMidPoint1D(function,s,p) - ThreePointMidPoint1D(function,2*s,p) )/double(3);
 }
-double Differentiation::NthDiff(double (*differentiator)(RealFunction1D, StepSize, Point), RealFunction1D rf,
+double Differentiation::NthDiff(double (*differentiator)(RealFunction1D, StepSize, Point), const RealFunction1D& rf,
                                 StepSize s, Point p, int n) {
 
     if(n>0){
@@ -27,4 +28,77 @@ double Differentiation::NthDiff(double (*differentiator)(RealFunction1D, StepSiz
     } else{
         return rf(p);
     }
+}
+
+
+RealFunction1D Interpolation::LagrangeCoefficient(int n,const  Point *a,int k_point) {
+
+    return [n,a,k_point](double x){
+        Point answer = 1;
+        Point k = a[k_point];
+        Point w = a[1];
+        for(int i = 0 ; i<n;i++){
+            if(i!=k_point){
+                answer=answer*((x - a[i])/(a[k_point]-a[i]));
+            }
+        }
+        return answer;
+    };
+}
+RealFunction1D Interpolation::LagrangePolynomialFromDataSetWithNPoint(int n, const Point *a,const  Point *y) {
+    auto func = [n,a,y](double x){
+        double answer = 0;
+        for(int i = 0 ; i<n;i++){
+            answer += (y[i])*(LagrangeCoefficient(n,a,i)(x));
+        }
+        return answer;
+    };
+    return func;
+}
+RealFunction1D Interpolation::LagrangePolynomialFromFunction(const RealFunction1D& function1D, int samples, Point start,
+                                                             Point end,Point x[],Point y[]) {
+    if(start>end){  // just to make sure a is after b :)
+        Point temp = start;
+        start = end;
+        end = temp;
+    }
+
+    double steps = (end - start)/(double)(samples);
+    for(int i=0;i<samples;i++){
+        x[i] = start+(steps*i);
+        y[i] = function1D(x[i]);
+    }
+    return LagrangePolynomialFromDataSetWithNPoint(samples,x,y);
+}
+
+RealFunction1D Integration::SimpleIndefinite(const RealFunction1D& function1D, Point lowerbound, double stepSize) {
+    return [function1D,lowerbound,stepSize](double x){
+        int chunks = std::abs(x-lowerbound)/stepSize;
+        double answer =0 ;
+        for(int i =0;i<chunks;i++){
+            answer += function1D(lowerbound + (i+0.5)*stepSize)*(stepSize);
+        }
+        return answer;
+    };
+}
+double Integration::SimpleDefinite(const RealFunction1D& function1D, Point LowerBound, Point UpperBound, double stepsize) {
+    return Integration::SimpleIndefinite(function1D,LowerBound,stepsize)(UpperBound);
+}
+
+RealFunction1D Integration::SimpsonIndefiniteClosed(const RealFunction1D& function1D, Point LowerBound, double stepSize) {
+    return [function1D,LowerBound,stepSize](double x){
+        int n = (x-LowerBound)/stepSize;
+        double answer = 0;
+        for(int i = 0 ;i<n;i++){
+            answer += (stepSize/(double)6)*(function1D(LowerBound + i*stepSize)
+                    + 4*function1D(LowerBound+(i+0.5)*stepSize)
+                    + function1D(LowerBound+(i+1)*stepSize));
+        }
+        return answer;
+    };
+}
+
+double Integration::SimpsonDefiniteClosed(const RealFunction1D& function1D, Point LowerBound, Point UpperBound,
+                                          double stepSize) {
+    return SimpleIndefinite(function1D,LowerBound,stepSize)(UpperBound);
 }
--- library.h
+++ library.h
@@ -9,13 +9,30 @@ typedef std::function<double(double)> RealFunction1D;
 
 class Differentiation{
 public:
-    static double Forward1D(RealFunction1D rf,StepSize s,Point p);
-    static double Backward1D(RealFunction1D rf,StepSize s,Point p);
-    static double ThreePointMidPoint1D(RealFunction1D rf,StepSize s,Point p);
-    static double FivePointMidPoint1D(RealFunction1D rf,StepSize s,Point p);
-    static double NthDiff(double (*differentiator)(RealFunction1D ,StepSize s,Point p),RealFunction1D rf,StepSize s,Point p,int n);
+    static double Forward1D(const RealFunction1D& rf,StepSize s,Point p);
+    static double Backward1D(const RealFunction1D& rf,StepSize s,Point p);
+    static double ThreePointMidPoint1D(const RealFunction1D& rf,StepSize s,Point p);
+    static double FivePointMidPoint1D(const RealFunction1D& rf,StepSize s,Point p);
+    static double NthDiff(double (*differentiator)(RealFunction1D ,StepSize s,Point p),const RealFunction1D& rf,StepSize s,Point p,int n);
 }
 ;
+
+class Interpolation{
+private:
+    static RealFunction1D LagrangeCoefficient(int n, const Point x[], int K_point);
+public:
+    static RealFunction1D LagrangePolynomialFromDataSetWithNPoint(int n,const Point x[] ,const Point y[]);
+    static RealFunction1D LagrangePolynomialFromFunction(const RealFunction1D& function1D,int samples,Point start,Point end,Point x[],Point y[]);
+};
+
+class Integration{
+public:
+    static RealFunction1D SimpleIndefinite(const RealFunction1D& function1D,Point LowerBound,double stepSize);
+    static double SimpleDefinite(const RealFunction1D& function1D,Point LowerBound,Point UpperBound,double stepsize);
+
+    static RealFunction1D SimpsonIndefiniteClosed(const RealFunction1D& function1D,Point LowerBound,double stepSize);
+    static double SimpsonDefiniteClosed(const RealFunction1D& function1D,Point LowerBound,Point UpperBound,double stepSize);
+};
 
 
 #endif //MATHUTILS_LIBRARY_H
